0a20e9e2d67712f525ac6d2d97221f97016ab444
Ingenio <Ingenio@192.168.2.23>
2016-05-25 11:50:21 -0500

    commit Funcionamiento correcto tarjetas imagen codigo codigo de barras

--- raizBarcode.php
+++ raizBarcode.php
@@ -1,22 +1,24 @@
 <?php 
+error_reporting(5); 
 require_once('classes/codeBar/barcode.class.php');
+
 $algo="";
 $barcode= new BARCODE();
 $barnumber=$_POST['codeImg2'];
-$ruta="upload/";
+$ruta="./upload/";
 $archivo="code-".$barnumber;
 $extension=".png";
 if (file_exists($ruta.$archivo.$extension)) unlink($ruta.$archivo.$extension);
         if (!empty($barnumber)) {
             
             $algo = $barcode->_c128Barcode($barnumber,1,$archivo,$ruta);
-            //$response = $file_path."<br/><img src='".$file_path."'>"; 
-            if(isset($algo)){
+            //if(isset($algo)){
                 
-              echo $algo;
-            }
-            else{
-                echo false;
-            }
-        }    
+              echo $ruta.$archivo.$extension;
+              
+            //}else{
+            //    echo false;
+            //}
+        }
      
+
