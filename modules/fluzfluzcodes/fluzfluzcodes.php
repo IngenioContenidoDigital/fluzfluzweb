fdf2a0ad317a9afc9c2b448df67e40051ec8d085
lfelipeqn <lfelipeqn@lfelipeqn-N53SM>
2016-07-03 08:44:59 -0500

    -Conteo de Codigos Asignados -Modificaciones en tpl

--- modules/fluzfluzcodes/fluzfluzcodes.php
+++ modules/fluzfluzcodes/fluzfluzcodes.php
@@ -51,10 +51,17 @@ class fluzfluzCodes extends Module{
     }
     
     public function hookdisplayAdminProductsExtra($params) {
-        $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->ExecuteS("SELECT code
+        $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->ExecuteS("SELECT "._DB_PREFIX_."product_code.`code`, (CASE "._DB_PREFIX_."product_code.id_order WHEN 0 THEN 'Disponible' ELSE 'Asignado' END) AS estado, CASE "._DB_PREFIX_."product_code.id_order WHEN 0 THEN '' ELSE "._DB_PREFIX_."product_code.id_order END AS `order` 
                     FROM ps_product_code
-                    WHERE id_product = ".Tools::getValue('id_product')."
-                    AND id_order = 0");
+                    WHERE id_product = ".Tools::getValue('id_product'));
+        
+        $totals = "SELECT rs.estado, count(rs.code) AS total
+FROM (SELECT pc.`code`, (CASE pc.id_order WHEN 0 THEN 'Disponible' ELSE 'Asignado' END) AS estado, CASE pc.id_order WHEN 0 THEN '' ELSE pc.id_order END AS `order` 
+                    FROM "._DB_PREFIX_."product_code AS pc
+                    WHERE id_product = ".Tools::getValue('id_product').") as rs GROUP BY rs.estado";
+        $rtotal = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($totals);
+        
+        $this->context->smarty->assign('totals', $rtotal );
         $this->context->smarty->assign('codes', $result );
         return $this->display(__FILE__, 'views/fluzfluzcodes_admin.tpl');
     }
--- modules/fluzfluzcodes/views/fluzfluzcodes_admin.tpl
+++ modules/fluzfluzcodes/views/fluzfluzcodes_admin.tpl
@@ -1,13 +1,24 @@
 <div class="panel">
     <h3>Codigo Productos</h3>
+    <p>{l s = 'Total de codigos asignados al producto seleccionado' mod="fluzfluzcodes"}</p>
+    {foreach $totals as $total}
+        <span>{$total.estado}:&nbsp;</span><span>{$total.total}</span>
+    {/foreach}
+    <br><br>
+    <div style="height: 450px; overflow-y: scroll;">
     <table border="1">
         <tr>
             <td style="text-align: center"><strong>Codigos</strong></td>
+            <td style="text-align: center"><strong>Estado</strong></td>
+            <td style="text-align: center"><strong>&nbsp;Orden&nbsp;</strong></td>
         </tr>
         {foreach $codes as $code}
             <tr>
                 <td>&nbsp;&nbsp;&nbsp;{$code.code}&nbsp;&nbsp;&nbsp;</td>
+                <td>&nbsp;&nbsp;&nbsp;{$code.estado}&nbsp;&nbsp;&nbsp;</td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;{$code.order}&nbsp;&nbsp;&nbsp;&nbsp;</td>
             </tr>
         {/foreach}
     </table>
+    </div>
 </div>
