6c168cec1085a1da1fa3f045d4cf428b662df449
JsonGmz <jeisson.gomez@ingeniocontenido.co>
2016-09-06 19:59:30 -0500

    -Imagenes comerciante -Vista comerciantes pagina categorias

--- controllers/admin/AdminManufacturersController.php
+++ controllers/admin/AdminManufacturersController.php
@@ -300,11 +300,24 @@ class AdminManufacturersControllerCore extends AdminController
         if (!($manufacturer = $this->loadObject(true))) {
             return;
         }
-
+        
+        // image logo
         $image = _PS_MANU_IMG_DIR_.$manufacturer->id.'.jpg';
         $image_url = ImageManager::thumbnail($image, $this->table.'_'.(int)$manufacturer->id.'.'.$this->imageType, 350,
             $this->imageType, true, true);
         $image_size = file_exists($image) ? filesize($image) / 1000 : false;
+        
+        // image medium
+        $imagemedium = _PS_MANU_IMG_DIR_."m/".$manufacturer->id.'.jpg';
+        $image_urlmedium = ImageManager::thumbnail($imagemedium, $this->table.'_medium_'.(int)$manufacturer->id.'.'.$this->imageType, 350,
+            $this->imageType, true, true);
+        $image_sizemedium = file_exists($imagemedium) ? filesize($imagemedium) / 1000 : false;
+        
+        // image banner
+        $imagebanner = _PS_MANU_IMG_DIR_."b/".$manufacturer->id.'.jpg';
+        $image_urlbanner = ImageManager::thumbnail($imagebanner, $this->table.'_banner_'.(int)$manufacturer->id.'.'.$this->imageType, 350,
+            $this->imageType, true, true);
+        $image_sizebanner = file_exists($imagebanner) ? filesize($imagebanner) / 1000 : false;
 
         $this->fields_form = array(
             'tinymce' => true,
@@ -352,6 +365,26 @@ class AdminManufacturersControllerCore extends AdminController
                     'display_image' => true,
                     'col' => 6,
                     'hint' => $this->l('Upload a manufacturer logo from your computer.')
+                ),
+                array(
+                    'type' => 'file',
+                    'label' => $this->l('Medium'),
+                    'name' => 'medium',
+                    'image' => $image_urlmedium ? $image_urlmedium : false,
+                    'size' => $image_sizemedium,
+                    'display_image' => true,
+                    'col' => 6,
+                    'hint' => $this->l('Upload a manufacturer medium from your computer.')
+                ),
+                array(
+                    'type' => 'file',
+                    'label' => $this->l('Banner'),
+                    'name' => 'banner',
+                    'image' => $image_urlbanner ? $image_urlbanner : false,
+                    'size' => $image_sizebanner,
+                    'display_image' => true,
+                    'col' => 6,
+                    'hint' => $this->l('Upload a manufacturer banner from your computer.')
                 ),
                 array(
                     'type' => 'text',
@@ -861,6 +894,30 @@ class AdminManufacturersControllerCore extends AdminController
     {
         if (Tools::isSubmit('submitAddaddress')) {
             $this->display = 'editaddresses';
+        } elseif (Tools::isSubmit('submitAddmanufacturer')) {
+            $id_manufacturer = (int)Tools::getValue('id_manufacturer');
+            if ( $_FILES['medium']['tmp_name'] != "" ) {
+                $typeimg = explode("/", $_FILES['medium']['type']);
+                if ( $typeimg[0] != "image" || ($typeimg[1] != "jpg" && $typeimg[1] != "jpg" ) ) {
+                    $this->errors[] = Tools::displayError('El archivo medium cargado no se encuentra en un formato correcto (JPG).');
+                } else {
+                    $target_path = _PS_MANU_IMG_DIR_ . "m/" . basename( $id_manufacturer.".".$typeimg[1] );
+                    if ( !move_uploaded_file($_FILES['medium']['tmp_name'], $target_path) ) {
+                        $this->errors[] = Tools::displayError('No fue posible cargar la imagen medium.');
+                    }
+                }
+            }
+            if ( $_FILES['banner']['tmp_name'] != "" ) {
+                $typeimg = explode("/", $_FILES['banner']['type']);
+                if ( $typeimg[0] != "image" || ($typeimg[1] != "jpg" && $typeimg[1] != "jpg" ) ) {
+                    $this->errors[] = Tools::displayError('El archivo banner cargado no se encuentra en un formato correcto (JPG).');
+                } else {
+                    $target_path = _PS_MANU_IMG_DIR_ . "b/" . basename( $id_manufacturer.".".$typeimg[1] );
+                    if ( !move_uploaded_file($_FILES['banner']['tmp_name'], $target_path) ) {
+                        $this->errors[] = Tools::displayError('No fue posible cargar la imagen banner.');
+                    }
+                }
+            }
         }
 
         return parent::processSave();
