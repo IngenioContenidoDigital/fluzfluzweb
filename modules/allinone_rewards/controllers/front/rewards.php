55865d9367d1945fb534c2a83d167a1b5e7dedf4
Ingenio <Ingenio@192.168.2.57>
2016-09-16 18:08:51 -0500

    pago con puntos totales funcionamiento correcto pruebas funcionales red ascedente y descendente puntos parciales (pendiente)

--- modules/allinone_rewards/controllers/front/rewards.php
+++ modules/allinone_rewards/controllers/front/rewards.php
@@ -88,7 +88,14 @@ class Allinone_rewardsRewardsModuleFrontController extends ModuleFrontController
                             $response=RewardsModel::createDiscount($money);
                             $query1 = "INSERT INTO "._DB_PREFIX_."rewards (id_reward_state, id_customer, id_order, id_cart, id_cart_rule, id_payment, credits, plugin, date_add, date_upd)"
                                     . "                          VALUES ('4', ".(int)$this->context->customer->id.", 0,".(int)$this->context->cart->id.",'0','0',".-1*$cartpoints.",'loyalty','".date("Y-m-d H:i:s")."', '".date("Y-m-d H:i:s")."')";
-                       Db::getInstance()->execute($query1);
+                            Db::getInstance()->execute($query1);
+                       }
+                       else if($points){
+                            
+                            $response=RewardsModel::createDiscount($money);
+                            $query1 = "INSERT INTO "._DB_PREFIX_."rewards (id_reward_state, id_customer, id_order, id_cart, id_cart_rule, id_payment, credits, plugin, date_add, date_upd)"
+                                    . "                          VALUES ('4', ".(int)$this->context->customer->id.", 0,".(int)$this->context->cart->id.",'0','0',".-1*$points.",'loyalty','".date("Y-m-d H:i:s")."', '".date("Y-m-d H:i:s")."')";
+                            Db::getInstance()->execute($query1);
                        }
                        else{
                             $response=RewardsModel::createDiscount($money);
