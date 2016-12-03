e94f5b93d62e8ac32b1e8909d6ae8f1c99a294df
Ubuntu <ubuntu@ip-10-249-4-137.sa-east-1.compute.internal>
2016-12-03 13:56:44 -0500

    -Merge de archivos

--- classes/order/Order.php
+++ classes/order/Order.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-include('codeBar/barcode.class.php');
+include('./classes/codeBar/barcode.class.php');
 
 class OrderCore extends ObjectModel
 {
