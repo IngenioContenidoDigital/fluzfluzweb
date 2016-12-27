f57a53ecd1cd668c78131ddc6c1cbdecabc8d64e
lfelipeqn <lfelipeqn@lfelipeqn-N53SM>
2016-12-26 19:01:31 -0500

    -Modificacion para carga de Archivos en S3. Se deben aplicar la reglas de direccionamiento.
    set $s3_img_bucket imagenes-fluzfluz/dev;
    
    rewrite ^/api/?(.*)$ /webservice/dispatcher.php?url=$1 last;
    
    rewrite ^/([0-9])(-[_a-zA-Z0-9-]*)?(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/p/$1$2.jpg last;
    rewrite ^/([0-9])([0-9])(-[_a-zA-Z0-9-]*)?(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/p/$1$2$3.jpg last;
    rewrite ^/([0-9])([0-9])([0-9])(-[_a-zA-Z0-9-]*)?(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/p/$1$2$3$4.jpg last;
    rewrite ^/([0-9])([0-9])([0-9])([0-9])(-[_a-zA-Z0-9-]*)?(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/p/$1$2$3$4$5.jpg last;
    rewrite ^/([0-9])([0-9])([0-9])([0-9])([0-9])(-[_a-zA-Z0-9-]*)?(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/p/$1$2$3$4$5$6.jpg last;
    rewrite ^/([0-9])([0-9])([0-9])([0-9])([0-9])([0-9])(-[_a-zA-Z0-9-]*)?(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/p/$1$2$3$4$5$6$7.jpg last;
    rewrite ^/([0-9])([0-9])([0-9])([0-9])([0-9])([0-9])([0-9])(-[_a-zA-Z0-9-]*)?(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/p/$1$2$3$4$5$6$7$8.jpg last;
    rewrite ^/([0-9])([0-9])([0-9])([0-9])([0-9])([0-9])([0-9])([0-9])(-[_a-zA-Z0-9-]*)?(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/p/$1$2$3$4$5$6$7$8$9.jpg last;
    
    rewrite ^/c/([0-9]+)(-[_a-zA-Z0-9-]*)(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/c/$1$2.jpg last;
    rewrite ^/c/([a-zA-Z-]+)(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/c/$1.jpg last;
    rewrite ^/([0-9]+)(-[_a-zA-Z0-9-]*)(-[0-9]+)?/.+\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/c/$1$2.jpg last;
    
    rewrite ^/images_ie/?([^/]+)\.(jpe?g|png|gif)$ /js/jquery/plugins/fancybox/images/$1.$2 last;
    
    rewrite ^/img/p/([0-9]/)*([_a-zA-Z0-9-]+)\.jpg$ https://s3.amazonaws.com/$s3_img_bucket/p/$2.jpg permanent;
    rewrite ^/img/(.+)$ https://s3.amazonaws.com/$s3_img_bucket/$1 permanent;

---
+++ override/classes/Aws.php
@@ -0,0 +1,112 @@
+<?php
+// Include the SDK using the Composer autoloader
+include_once("../tools/aws/aws-autoloader.php");
+use Aws\S3\MultipartUploader;
+use Aws\Exception\MultipartUploadException;
+
+/**
+ * Conexión servicio amazon web service
+ */
+class AwsCore extends ObjectModel
+{
+        public $s3Client;
+        public $bucket = 'imagenes-fluzfluz';
+        public $shopDomainProd = 'fluzfluz.co';
+
+        /**
+         * Datos de acceso
+         */
+        public function __construct()
+        {
+                $this->s3Client = new Aws\S3\S3Client([
+                  'version' => 'latest',
+                  'region'  => 'us-east-1',
+                  'credentials' => [
+                        'key'    => 'AKIAJQ45NVDTHPITK4CQ',
+                        'secret' => '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',
+                    ],
+                ]);
+        }
+
+        /**
+         * Recupera objetos de Amazon S3
+         *
+         * @param obj string nombre de objeto
+         * @return object 
+         */
+        public function getObject($obj = '', $bucketType = '')
+        {
+                $result = $this->s3Client->getObject([
+                    'Bucket' => $this->bucket . $bucketType,
+                    'Key'    => $obj
+                ]);
+
+                return $result;
+        }
+
+        /**
+         * Obtiene la URL de un objeto
+         * Este método devuelve una URL sin firmar para el bucket y key.
+         *
+         * @param obj string nombre de objeto
+         * @return string URL del objeto
+         */
+        public function getObjectUrl($obj = '', $bucketType = '')
+        {
+                return $this->s3Client->getObjectUrl($this->bucket . $bucketType, $obj);
+        }
+
+        /**
+         * Subidas de múltiples partes están diseñadas 
+         * para mejorar la experiencia de carga de objetos más grandes
+         * Nota: Se recomienda a los clientes de Amazon S3 
+         * que utilicen subidas múltiples para objetos de más de 100 MB.
+         *
+         * @see https://docs.aws.amazon.com/aws-sdk-php/v3/guide/service/s3-multipart-upload.html
+         * @param obj string ruta del objeto
+         * @return object 
+         */
+        public function setObject($srcObj = '', $obj = '', $bucketType = '')
+        {
+                $uploader = new MultipartUploader($this->s3Client, $srcObj, [
+                        'bucket' => $this->bucket . $bucketType,
+                        'key'    => $obj,
+                        'acl'    => 'public-read'  
+                ]);
+
+                try {
+                        return $uploader->upload();
+                } catch (MultipartUploadException $e) {
+                        return $e->getMessage() . "\n";
+                }
+        }
+
+        /**
+         * Carga un objeto de hasta 5 GB
+         *
+         * @see http://docs.aws.amazon.com/AmazonS3/latest/dev/UploadObjSingleOpPHP.html
+         * @param srcObj string ruta del objeto
+         * @param obj string nombre del objeto
+         * @param folder string directorio en el bucket
+         * @return object
+         */
+        public function setObjectImage($srcObj = '', $obj = '', $folder = '')
+        {
+                $folder = Configuration::get('PS_SHOP_DOMAIN') == $this->shopDomainProd ? $folder : 'dev/' . $folder;
+                try {
+                        $result = $this->s3Client->putObject(array(
+                                'Bucket' => $this->bucket,
+                                'Key' => $folder . $obj, 
+                                'SourceFile' => $srcObj,
+                                'ACL' => 'public-read', 
+                                'CacheControl' => 'public',
+                                'Expires' => strtotime("+180 day")
+                        ));
+                        return $result['ObjectURL'];
+                } catch (Exception $e) {
+                    return false;
+                }
+        }
+}
+
+
