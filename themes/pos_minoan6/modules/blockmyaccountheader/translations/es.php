cf33f400507ef93eb9d18a24670bd74712e37f91
Ubuntu <ubuntu@ip-10-249-46-85.sa-east-1.compute.internal>
2017-01-18 13:57:26 -0500

    Actualizacion de Traducciones Red / Network - Puntos / Fluz

--- themes/pos_minoan6/modules/blockmyaccountheader/translations/es.php
+++ themes/pos_minoan6/modules/blockmyaccountheader/translations/es.php
@@ -7,8 +7,8 @@ $_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_9cfeea9e5661b2
 $_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_bea8d9e6a0d57c4a264756b4f9822ed9'] = 'Mi Cuenta';
 $_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_89080f0eedbd5491a93157930f1e45fc'] = 'Devoluciones';
 $_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_4e997b9d3b7152f2afeacd96ab1ef1cb'] = 'Mis tarjetas';
-$_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_c2b1ef45417a2aac50465e2092e942da'] = 'Mi red';
-$_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_edbf7d305f174d743ff65f18adb4bdb1'] = 'Mi red';
+$_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_c2b1ef45417a2aac50465e2092e942da'] = 'Mi network';
+$_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_edbf7d305f174d743ff65f18adb4bdb1'] = 'Mi network';
 $_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_e4cdf236281cce8a3c0742cb493c5f82'] = 'Invita a un amigo';
 $_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_2d31abc75971a57e48bbda9e00abe9cb'] = 'Redimir';
 $_MODULE['<{blockmyaccountheader}pos_minoan6>blockmyaccountheader_95d2137c196c7f84df5753ed78f18332'] = 'Mis Cupones';
