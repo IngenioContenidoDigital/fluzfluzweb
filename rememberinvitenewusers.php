19ae95c87e9c7f388978679ede70839d2410e3f9
JsonGmz <jeisson.gomez@ingeniocontenido.co>
2017-01-18 12:13:48 -0500

    FLUZ-261 -Email recordatorio invitar nuevos usuarios

---
+++ rememberinvitenewusers.php
@@ -0,0 +1,35 @@
+<?php 
+include_once('./config/defines.inc.php');
+include_once('./config/config.inc.php');
+
+$query = "SELECT c.id_customer, c.username, c.email, COUNT(rs.id_sponsor) invitation_count
+            FROM "._DB_PREFIX_."customer c
+            LEFT JOIN "._DB_PREFIX_."rewards_sponsorship rs ON ( c.id_customer = rs.id_sponsor )
+            GROUP BY c.id_customer
+            HAVING invitation_count < 2";
+
+$customers = Db::getInstance()->executeS($query);
+
+//echo '<pre>'; print_r($customers); die();
+
+foreach ( $customers as $key => $customer ) {
+    $vars = array(
+        '{username}' => $customer['username'],
+        '{invitation_count}' => $customer['invitation_count'],
+        '{shop_name}' => Configuration::get('PS_SHOP_NAME'),
+        '{shop_url}' => Context::getContext()->link->getPageLink('index', true, Context::getContext()->language->id, null, false, Context::getContext()->shop->id)
+    );
+
+    Db::getInstance()->execute("INSERT INTO "._DB_PREFIX_."notification_history(id_customer, type_message, message, date_send)
+                                VALUES (".$customer['id_customer'].",'Invita un nuevo Fluzzer', 'Te informamos que aun tienes ".$customer['invitation_count']." invitacion (es) pendiente(s).', NOW())");
+
+    Mail::Send(
+        Context::getContext()->language->id,
+        'rememberinvitenewusers',
+        'Invita un nuevo Fluzzer',
+        $vars,
+        $customer['email'],
+        $customer['username']
+    );
+}
+
