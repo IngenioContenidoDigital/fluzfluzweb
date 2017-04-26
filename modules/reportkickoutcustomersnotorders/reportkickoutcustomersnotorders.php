68a16d1d16be65d11a710c3ab9c00e0321de389c
JsonGmz <jeisson.gomez@ingeniocontenido.co>
2017-04-26 13:23:21 -0500

    FLUZ-561 - Reporte clientes sin ordenes

---
+++ modules/reportkickoutcustomersnotorders/reportkickoutcustomersnotorders.php
@@ -0,0 +1,114 @@
+<?php
+
+if (!defined('_PS_VERSION_'))
+    exit;
+
+class reportkickoutcustomersnotorders extends ModuleGrid
+{
+    private $html;
+    private $query;
+    private $columns;
+
+    public function __construct()
+    {
+        $this->name = 'reportkickoutcustomersnotorders';
+        $this->tab = 'analytics_stats';
+        $this->version = '1.0';
+        $this->author = 'Ingenio Contenido Digital SAS';
+
+        parent::__construct();
+
+        $this->columns = array(
+            array(
+                'id' => 'id_customer',
+                'header' => $this->l('ID'),
+                'dataIndex' => 'id_customer',
+                'align' => 'center'
+            ),
+            array(
+                'id' => 'username',
+                'header' => $this->l('Usuario'),
+                'dataIndex' => 'username',
+                'align' => 'center'
+            ),
+            array(
+                'id' => 'email',
+                'header' => $this->l('Email'),
+                'dataIndex' => 'email',
+                'align' => 'center'
+            ),
+            array(
+                'id' => 'name',
+                'header' => $this->l('Cliente'),
+                'dataIndex' => 'name',
+                'align' => 'center'
+            ),
+            array(
+                'id' => 'date_kick_out',
+                'header' => $this->l('Fecha Expulsion'),
+                'dataIndex' => 'date_kick_out',
+                'align' => 'center'
+            ),
+        );
+
+        $this->displayName = $this->l('Reporte Clientes Expulsados Sin Ordenes');
+        $this->description = $this->l('Reporte Clientes Expulsados Sin Ordenes');
+    }
+
+    public function install()
+    {
+        return (parent::install() &&
+            $this->registerHook('AdminStatsModules'));
+    }
+
+    public function getData()
+    {
+        $date_between = $this->getDate();
+        $this->query = "SELECT
+                            ko.id_customer,
+                            c.username,
+                            ko.email,
+                            CONCAT(ko.firstname,' ',ko.lastname) name,
+                            ko.date_kick_out
+                        FROM "._DB_PREFIX_."rewards_sponsorship_kick_out ko
+                        LEFT JOIN "._DB_PREFIX_."customer c ON ( ko.id_customer = c.id_customer )
+                        LEFT JOIN "._DB_PREFIX_."orders o ON ( ko.id_customer = o.id_customer )
+                        WHERE o.id_order IS NULL
+                        AND ko.date_kick_out BETWEEN ".$date_between;
+
+        $list = Db::getInstance()->executeS($this->query);
+
+        if ( $list[0]['id_customer'] != "" ) {
+            $this->_values = $list;
+        }
+    }
+
+    public function hookAdminStatsModules($params)
+    {
+        $this->context->smarty->assign(array(
+            'displayName' => $this->displayName,
+        ));
+
+        $engine_params = array(
+            'id' => 'id_customer',
+            'title' => $this->displayName,
+            'columns' => $this->columns
+        );
+
+        if (Tools::getValue('export')) {
+            $this->csvExport($engine_params);
+        }
+
+        $this->html = '
+		<div class="panel-heading">
+			'.$this->displayName.'
+		</div>
+                '.$this->display(__FILE__, 'AdminStatsModules.tpl').'
+		'.$this->engine($engine_params).'
+		<a class="btn btn-default export-csv" href="'.Tools::safeOutput($_SERVER['REQUEST_URI'].'&export=').'1">
+			<i class="icon-cloud-upload"></i> '.$this->l('CSV Export').'
+		</a>';
+
+        return $this->html;
+    }
+}
