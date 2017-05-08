39b1a7001bc6dd81f1c44936082c1e20a5e67eb4
JsonGmz <jeisson.gomez@ingeniocontenido.co>
2017-05-08 12:47:15 -0500

    Order.php error merge

--- override/classes/order/Order.php
+++ override/classes/order/Order.php
@@ -292,33 +292,6 @@ class Order extends OrderCore
         $history->addWithemail();
     }
     
-    public function setCurrentState($id_order_state, $id_employee = 0)
-    {
-        if (empty($id_order_state)) {
-            return false;
-        }
-        $history = new OrderHistory();
-        $history->id_order = (int)$this->id;
-        $history->id_employee = (int)$id_employee;
-        $history->changeIdOrderState((int)$id_order_state, $this);
-        $res = Db::getInstance()->getRow('
-			SELECT `invoice_number`, `invoice_date`, `delivery_number`, `delivery_date`
-			FROM `'._DB_PREFIX_.'orders`
-			WHERE `id_order` = '.(int)$this->id);
-        $this->invoice_date = $res['invoice_date'];
-        $this->invoice_number = $res['invoice_number'];
-        $this->delivery_date = $res['delivery_date'];
-        $this->delivery_number = $res['delivery_number'];
-        $this->update();
-        
-        if ( $id_order_state == 2 && $id_employee == 0 ) {
-            $ordercodes = new Order((int)$this->id);
-            $this->updateCodes($ordercodes);
-        }
-
-        $history->addWithemail();
-    }
-    
     public static function getEmailTemplateContent($template_name, $mail_type, $var)
     {
         $context = Context::getContext();
