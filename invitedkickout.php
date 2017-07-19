b05e1678bf7325d93f5b72055f73dc5a84fafc43
ingenio0 <ingenio0@ingenio0-MS-7817>
2017-07-19 10:47:07 -0500

    Reenvio Invitaciones Usuarios Expulsados. 23

--- invitedkickout.php
+++ invitedkickout.php
@@ -5,8 +5,7 @@ include_once('./modules/allinone_rewards/allinone_rewards.php');
 include_once('./modules/allinone_rewards/models/RewardsSponsorshipModel.php');
 include_once('./modules/allinone_rewards/controllers/front/sponsorship.php');
 
-$tree_s = array('193','1023','4907','51','1222','892','1246','446','440','626',
-                '4227','450','1165','4616','5793','4617','351','7571','367','1526',
+$tree_s = array('7571','367','1526',
                 '1264','372','1579','1818','1231','4404','503','275','230','992');
 $network = array();
 
