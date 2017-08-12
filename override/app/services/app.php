fc31776e07234f2aba50056d87a841be37ce72aa
JsonGmz <jeisson.gomez@ingeniocontenido.co>
2017-08-11 21:49:34 -0500

    Funcion Create New Customer

--- override/app/services/app.php
+++ override/app/services/app.php
@@ -688,20 +688,25 @@ class API extends REST {
     }
     
     private function creationCustomer() {
-            
+        if ($this->get_request_method() != "POST") {
+          $this->response('', 406);
+        }
+        
         $array_error = array();
 
-        $firstname = $this->_request['firstname'];
-        $lastname = $this->_request['lastname'];
-        $username = $this->_request['username'];
-        $birthday = $this->_request['birthday'];
+        $firstname = $this->_request['firts_name'];
+        $lastname = $this->_request['last_name'];
         $email = $this->_request['email'];
-        $dni = $this->_request['dni'];
-        $type_dni = $this->_request['type_dni'];
-        $addres1 = $this->_request['address1'];
-        $addres2 = $this->_request['address2'];
-        $city = $this->_request['city'];
         $phone = $this->_request['phone'];
+        $birthday = $this->_request['date'];
+        $addres1 = $this->_request['address'];
+        $city = $this->_request['city'];
+        $type_dni = $this->_request['type_identification'];
+        $dni = $this->_request['number_identification'];
+        $username = $this->_request['user_name'];
+        $addres2 = $this->_request['address2'];
+        $response = array('success' => true);
+        
 
         $valid_dni = Db::getInstance()->getRow('SELECT COUNT(dni) as dni 
                                                 FROM '._DB_PREFIX_.'customer WHERE dni = "'.$dni.'" ');
@@ -710,19 +715,19 @@ class API extends REST {
                                                 FROM '._DB_PREFIX_.'customer WHERE username = "'.$username.'" ');
 
         if (empty($firstname) || empty($lastname) || !Validate::isName($firstname) || !Validate::isName($lastname)) {
-            $error['name'] = 'name invalid';
+            $error['name'] = 'Nombre o apellido invalido.';
         } elseif (!Validate::isEmail($email)) {
-            $error['email'] = 'email invalid';
+            $error['email'] = 'El correo electronico es invalido.';
         } elseif (empty($dni)) {
-            $error['dni'] = 'No se ha ingresado correctamente el campo Cedula';
+            $error['dni'] = 'No se ha ingresado correctamente el campo Cedula.';
         } elseif ($valid_dni['dni'] > 0) {
-            $error['dni_exists'] = 'dni exists';
+            $error['dni_exists'] = 'El numero de identificacion es invalido.';
         } elseif ($valid_username['username'] > 0) {
-            $error['valid_username'] = 'valid username';
+            $error['valid_username'] = 'El nombre de usuario se encuentra en uso.';
         } elseif (RewardsSponsorshipModel::isEmailExists($email) || Customer::customerExists($email)) {
             $customerKickOut = Db::getInstance()->getValue("SELECT kick_out FROM "._DB_PREFIX_."customer WHERE email = '".$email."'");
             if ($customerKickOut == 0) {
-                $error['email_exists'] = 'email exists';
+                $error['email_exists'] = 'El correo electronico se encuentra en uso.';
             }
         }
 
@@ -743,83 +748,85 @@ class API extends REST {
             $customer->username = $username;
             $customer->birthday = $birthday;
             $customer->id_default_group = 4;
+            $customer->kick_out = 0;
+            $customer->active = 1;
             $customer->id_lang = Context::getContext()->language->id;
             $customer->date_kick_out = date('Y-m-d H:i:s', strtotime('+30 day', strtotime(date("Y-m-d H:i:s"))));
-            $customer->add();
 
-            $address = new Address();
-            $address->id_country = 69;
-            $address->id_customer = $customer->id;
-            $address->alias = 'Mi Direccion';
-            $address->firstname = $customer->firstname;
-            $address->lastname = $customer->lastname;
-            $address->address1 = $addres1;
-            $address->address2 = $addres2;
-            $address->city = $city;
-            $address->phone = $phone;
-            $address->phone_mobile = $phone;
-            $address->type_document = $type_dni;
-            $address->active = 1;
-            $address->add();
+            if( $customer->add() ){
+              $address = new Address();
+              $address->id_country = 69;
+              $address->dni = $customer->dni;
+              $address->id_customer = $customer->id;
+              $address->alias = 'Mi Direccion';
+              $address->firstname = $customer->firstname;
+              $address->lastname = $customer->lastname;
+              $address->address1 = $addres1;
+              $address->address2 = $addres2;
+              $address->city = $city;
+              $address->phone = $phone;
+              $address->phone_mobile = $phone;
+              $address->type_document = $type_dni;
+              $address->active = 1;
 
-            $query = 'SELECT
-                            c.id_customer,
-                            c.username,
-                            c.email,
-                            (2 - COUNT(rs.id_sponsorship)) pendingsinvitation
-                        FROM '._DB_PREFIX_.'customer c
-                        LEFT JOIN '._DB_PREFIX_.'rewards_sponsorship rs ON ( c.id_customer = rs.id_sponsor )
-                        LEFT JOIN '._DB_PREFIX_.'customer_group cg ON ( c.id_customer = cg.id_customer AND cg.id_group = 4 )
-                        WHERE c.active = 1
-                        AND c.kick_out = 0
-                        GROUP BY c.id_customer
-                        HAVING pendingsinvitation > 0
-                        ORDER BY c.id_customer ASC
-                        LIMIT 1';
+              $query = 'SELECT
+                              c.id_customer,
+                              c.username,
+                              c.email,
+                              (2 - COUNT(rs.id_sponsorship)) pendingsinvitation
+                          FROM '._DB_PREFIX_.'customer c
+                          LEFT JOIN '._DB_PREFIX_.'rewards_sponsorship rs ON ( c.id_customer = rs.id_sponsor )
+                          LEFT JOIN '._DB_PREFIX_.'customer_group cg ON ( c.id_customer = cg.id_customer AND cg.id_group = 4 )
+                          WHERE c.active = 1
+                          AND c.kick_out = 0
+                          GROUP BY c.id_customer
+                          HAVING pendingsinvitation > 0
+                          ORDER BY c.id_customer ASC
+                          LIMIT 1';
 
-            $sponsor = Db::getInstance()->ExecuteS($query);
-            $sponsor = $sponsor[0];
+              $sponsor = Db::getInstance()->ExecuteS($query);
+              $sponsor = $sponsor[0];
 
-            if ( !empty($sponsor) ) {
+              if ( !empty($sponsor) && $address->add() ) {
 
-                $sponsorship = new RewardsSponsorshipModel();
-                $sponsorship->id_sponsor = $rows['id_customer'];
-                $sponsorship->id_customer = $customer->id;
-                $sponsorship->firstname = $customer->firstname;
-                $sponsorship->lastname = $customer->lastname;
-                $sponsorship->email = $customer->email;
-                $sponsorship->channel = 1;
+                  $sponsorship = new RewardsSponsorshipModel();
+                  $sponsorship->id_sponsor = $sponsor['id_customer'];
+                  $sponsorship->id_customer = $customer->id;
+                  $sponsorship->firstname = $customer->firstname;
+                  $sponsorship->lastname = $customer->lastname;
+                  $sponsorship->email = $customer->email;
+                  $sponsorship->channel = 1;
 
-                if ($sponsorship->save()) {
+                  if ($sponsorship->save()) {
 
-                    $vars = array(
-                        '{username}' => $customer->username,
-                        '{password}' => $customer->dni,
-                        '{firstname}' => $customer->firstname,
-                        '{lastname}' => $customer->lastname,
-                        '{dni}' => $customer->dni,
-                        '{birthdate}' => $customer->birthday,
-                        '{address}' => $address->address1,
-                        '{phone}' => $address->phone,
-                        '{shop_name}' => Configuration::get('PS_SHOP_NAME'),
-                        '{shop_url}' => Context::getContext()->link->getPageLink('index', true, Context::getContext()->language->id, null, false, Context::getContext()->shop->id),
-                        '{shop_url_personal}' => Context::getContext()->link->getPageLink('identity', true, Context::getContext()->language->id, null, false, Context::getContext()->shop->id),
-                        '{learn_more_url}' => "http://reglas.fluzfluz.co",
-                    );
+                      $vars = array(
+                          '{username}' => $customer->username,
+                          '{password}' => $customer->dni,
+                          '{firstname}' => $customer->firstname,
+                          '{lastname}' => $customer->lastname,
+                          '{dni}' => $customer->dni,
+                          '{birthdate}' => $customer->birthday,
+                          '{address}' => $address->address1,
+                          '{phone}' => $address->phone,
+                          '{shop_name}' => Configuration::get('PS_SHOP_NAME'),
+                          '{shop_url}' => Context::getContext()->link->getPageLink('index', true, Context::getContext()->language->id, null, false, Context::getContext()->shop->id),
+                          '{shop_url_personal}' => Context::getContext()->link->getPageLink('identity', true, Context::getContext()->language->id, null, false, Context::getContext()->shop->id),
+                          '{learn_more_url}' => "http://reglas.fluzfluz.co",
+                      );
 
-                    $template = 'welcome_fluzfluz';
-                    $prefix_template = '16-welcome_fluzfluz';
+                      $template = 'welcome_fluzfluz';
+                      $prefix_template = '16-welcome_fluzfluz';
 
-                    $query_subject = 'SELECT subject_mail FROM '._DB_PREFIX_.'mail_send WHERE name_mail ="'.$prefix_template.'"';
-                    $row_subject = Db::getInstance()->getRow($query_subject);
-                    $message_subject = $row_subject['subject_mail'];
+                      $query_subject = 'SELECT subject_mail FROM '._DB_PREFIX_.'mail_send WHERE name_mail ="'.$prefix_template.'"';
+                      $row_subject = Db::getInstance()->getRow($query_subject);
+                      $message_subject = $row_subject['subject_mail'];
 
-                    $allinone_rewards = new allinone_rewards();
-                    $allinone_rewards->sendMail(Context::getContext()->language->id, $template, $allinone_rewards->getL($message_subject),$vars, $sponsorship->email, $customer->firstname.' '.$customer->lastname);
+                      $allinone_rewards = new allinone_rewards();
+                      $allinone_rewards->sendMail(Context::getContext()->language->id, $template, $allinone_rewards->getL($message_subject),$vars, $sponsorship->email, $customer->firstname.' '.$customer->lastname);
 
-                }
+                  }
+              }
             }
-            $response = array('success' => true);
         } else {
             $response = array('success' => false, 'error' => $error);
         }
