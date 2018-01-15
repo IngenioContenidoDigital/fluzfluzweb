9402a58fe85037bbca65ebe396997aa6d12eec27
ingenio0 <ingenio0@ingenio0-MS-7817>
2018-01-15 18:23:00 -0500

    Opciones banner web movil web/movil Backoffice

--- modules/posslideshow/models/Nivoslideshow.php
+++ modules/posslideshow/models/Nivoslideshow.php
@@ -2,14 +2,17 @@
 class Nivoslideshow extends ObjectModel
 {
  	/** @var string Name */
-		public $title;
+	public $title;
         public $description;
         public $image;
         public $link;
         public $porder;
-		public $active;
+	public $active;
+        public $type_route;
+        public $type_view;
+        public $link_app;
 
-	/**
+        /**
 	 * @see ObjectModel::$definition
 	 */
 	public static $definition = array(
@@ -17,13 +20,16 @@ class Nivoslideshow extends ObjectModel
 		'primary' => 'id_pos_slideshow',
 		'multilang' => TRUE,
 		'fields' => array(
-		            'porder' =>           array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'required' => false),
-					'active' =>           array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'required' => false),
-					'image' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isString', 'required' => false, 'size' => 3999999999),
-					//lang field
-                    'title' => array('type' => self::TYPE_STRING,'lang' => true, 'validate' => 'isGenericName', 'required' => false, 'size' => 265),
-					'link' => array('type' => self::TYPE_STRING,'lang' => true, 'validate' => 'isGenericName', 'required' => false, 'size' => 265),
-                    'description' => array('type' => self::TYPE_HTML, 'lang' => true, 'validate' => 'isString', 'size' => 3999999999999),
+		'porder' =>           array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'required' => false),
+                'active' =>           array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt', 'required' => false),
+	        'image' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isString', 'required' => false, 'size' => 3999999999),
+		//lang field
+                'type_route' =>           array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
+                'type_view' =>           array('type' => self::TYPE_INT, 'validate' => 'isUnsignedInt'),
+                'title' => array('type' => self::TYPE_STRING,'lang' => true, 'validate' => 'isGenericName', 'required' => false, 'size' => 265),
+                'link' => array('type' => self::TYPE_STRING,'lang' => true, 'validate' => 'isGenericName', 'required' => false, 'size' => 265),
+                'link_app' => array('type' => self::TYPE_STRING,'validate' => 'isGenericName'),
+                'description' => array('type' => self::TYPE_HTML, 'lang' => true, 'validate' => 'isString', 'size' => 3999999999999),
                 ),
 	);
 }
