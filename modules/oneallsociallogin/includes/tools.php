65a0d3af4dbcf34cd060da26e1f1e0e8bf4bc001
Ubuntu <faber.herrera@ingeniocontenido.co>
2018-02-20 18:00:06 -0500

    archivos

--- modules/oneallsociallogin/includes/tools.php
+++ modules/oneallsociallogin/includes/tools.php
@@ -261,6 +261,7 @@ class oneall_social_login_tools
                                                 WHERE method_add = "Backoffice" AND active = 1 ORDER BY date_from ASC LIMIT 1) AND NOW() 
                                                 AND method_add = "Backoffice" AND active = 1
                                                    ');
+
                 if($verified_reward[0]['id_reward_fluz'] != ''){
 
                     $reward = new RewardsModel();
@@ -270,8 +271,7 @@ class oneall_social_login_tools
                     $reward->credits = $verified_reward[0]['credits_back'];
                     $reward->reason = 'Recompensa FluzFluz';
                     $reward->date_add = date('Y-m-d H:i:s', strtotime('+0 day', strtotime(date("Y-m-d H:i:s"))));
-                    $reward->add();
-
+                    $reward->add()      
                 }
                 
                 $vars = array(
